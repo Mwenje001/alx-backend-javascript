d99e61eb6c51203df6d3218938fdf3f00ec39838
root <root@DESKTOP-EK7JHBM>
2023-07-05 05:18:59 +0300

    Initial Commits

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,13 @@
-export default function getListStudents() {
+/* eslint-disable no-unused-vars */
+export default function getListStudentIds(obj) {
   const myArr = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-  return myArr;
+  const mappedArr = myArr.map((element) => element.id);
+  if (Array.isArray(obj)) {
+    return mappedArr;
+  }
+  return [];
 }
--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,8 +1,8 @@
-export default function getListStudents() {
+export default function getStudentsByLocation(getListStudents, city) {
   const myArr = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-  return myArr;
+  return myArr.filter((myArr) => myArr.location === city);
 }
